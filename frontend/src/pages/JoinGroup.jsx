d1d2fc563261d3fb79708231bd66b2c7f311237c
Evan <evantreadwell228@gmail.com>
2026-04-26 14:44:04 -0400

    removed debug

--- frontend/src/pages/JoinGroup.jsx
+++ frontend/src/pages/JoinGroup.jsx
@@ -49,13 +49,6 @@ export default function JoinGroup() {
     }
 
     useEffect(() => {
-    // temporary debug
-    fetch(`${import.meta.env.VITE_REACT_APP_API_URL}/groups/debug`, {
-        credentials: "include"
-    })
-    .then(r => r.json())
-    .then(data => console.log("DEBUG:", data))
-
     fetchRecommendedGroups()
     fetchAllGroups()
 }, []);
